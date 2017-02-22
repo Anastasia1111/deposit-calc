68dd64577b4cf5b432c4d5d968bb00b8c1c0f700
Anastasia <Stranno@ngs.ru>
2017-02-22 15:00:21 +0700

    progdeposit pravka2

--- calcb.c
+++ calcb.c
@@ -8,15 +8,18 @@ int main()
     scanf("%f",&x);
     printf ("Введите срок в днях  ");
     scanf ("%i",&s);
-    if (x<=100) if (s<=30) x*=0.9;
-               else if (s<=120) x*=1.02;
-                    else if (s<=240) x*=1.06;
-                         else x*=1.12 ;
-    else if (s<=30) x*=0.9;
-               else if (s<=120) x*=1.03;
-                    else if (s<=240) x*=1.08;
-                         else x*=1.15;
-    
-    printf("%f",x);
+    if ((x>=10)&&(s>=0)&&(s<=365))
+             {
+              if (x<=100) if (s<=30) x*=0.9;
+                          else if (s<=120) x*=1.02;
+                               else if (s<=240) x*=1.06;
+                                    else x*=1.12 ;
+              else if (s<=30) x*=0.9;
+                   else if (s<=120) x*=1.03;
+                        else if (s<=240) x*=1.08;
+                             else x*=1.15;
+             printf("%f т.\n",x);
+             }
+    else printf("Ошибка, неправильно введены данные\n");    
     return 0;
 }
